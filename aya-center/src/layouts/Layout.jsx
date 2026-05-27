b334b0e472b7bf8216f17987a6414edd9dc96c00
Dibakar204 <dibakar.sarkar.204@gmail.com>
2026-05-27 12:36:02 +0530

    remove vercel

--- aya-center/src/layouts/Layout.jsx
+++ aya-center/src/layouts/Layout.jsx
@@ -1,6 +1,4 @@
 import { Outlet } from 'react-router-dom'
-
-import { SpeedInsights } from "@vercel/speed-insights/next"
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 
